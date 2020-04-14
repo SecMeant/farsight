b2ca3f6c706578a2920e381ac4f1d9b4493807c9
SecMeant <secmeant@gmail.com>
2020-04-14 11:38:12 +0200

    Looking around kinda works, wsad kinda less works
    moving forward and backwards aka w and s seems to work fine,
    however a and d is bugged as hell
    
    Looking around seems to work but mouse locking within window doesnt.

--- expr/3dview/src/main.cc
+++ expr/3dview/src/main.cc
@@ -11,11 +11,17 @@
 
 typedef float point3[3];
 
-static GLfloat viewer[] = { 5.0, 0.0, 0.0 };
-static GLfloat thetax = 0.0;
-static GLfloat thetay = 0.0;
-static GLfloat pix2anglex;
-static GLfloat pix2angley;
+static GLfloat viewer[] = { 0.0, 0.0, 0.0 };
+constexpr static GLfloat viewer_up[] = { 0.0, -1.0, 0.0 };
+static GLfloat mouse_sens = 1.0f / std::pow(2.0f, 9.0f);
+constexpr static GLfloat mouse_horizontal_max = M_PI * 2.0;
+constexpr static GLfloat mouse_horizontal_min = 0;
+constexpr static GLfloat mouse_vertical_max = M_PI * 2.0;
+constexpr static GLfloat mouse_vertical_min = 0;
+static GLfloat mouse_horizontal = mouse_horizontal_max / 2.0;
+static GLfloat mouse_vertical = mouse_vertical_max / 2.0;
+static GLfloat lookat[] = { 0.0, 0.0, 1.0f };
+static bool lock_mouse = true;
 
 static GLint status = 0;
 
@@ -29,10 +35,6 @@ static GLfloat scale = 1;
 
 static GLfloat offset_x = 0;
 static GLfloat offset_y = 0;
-
-static GLfloat lookpoint_x = 0;
-static GLfloat lookpoint_y = 0;
-static GLfloat lookpoint_z = 0;
 
 float data_x, data_y, data_width;
 std::vector<float> points;
@@ -97,6 +99,14 @@ drawpoints(const std::vector<float> &v)
   const float *data = v.data();
   size_t h = v.size() / w;
 
+  glBegin(GL_QUADS);
+  glColor3f(0.75f, 0.0f, 0.35f);
+  glVertex3f(0.0f, 0.0f, 0.0f);
+  glVertex3f(h, 0.0f, 0.0f);
+  glVertex3f(h, 0.0f, w);
+  glVertex3f(0.0f, 0.0f, w);
+  glEnd();
+
   glBegin(GL_POINTS);
   glColor3f(1.0f, 1.0f, 1.0f);
 
@@ -151,23 +161,17 @@ RenderScene(void)
   gluLookAt(viewer[0],
             viewer[1],
             viewer[2],
-            lookpoint_x,
-            lookpoint_y,
-            lookpoint_z,
-            0.0,
-            1.0,
-            0.0);
+            viewer[0] + lookat[0],
+            viewer[1] + lookat[1],
+            viewer[2] + lookat[2],
+            viewer_up[0],
+            viewer_up[1],
+            viewer_up[2]);
 
-  if (status == 1)
-  {
-    thetax += delta_x * pix2anglex;
-    thetay += delta_y * pix2angley;
-  }
-
-  glRotatef(thetax, 0.0, 1.0, 0.0);
-  glRotatef(thetay, 1.0, 0.0, 0.0);
-  glScalef(scale * 1.0, scale * 1.0, scale * 1.0);
-  glTranslatef(offset_x, offset_y, 0.0);
+  //glRotatef(thetax, 0.0, 1.0, 0.0);
+  //glRotatef(thetay, 1.0, 0.0, 0.0);
+  //glScalef(scale * 1.0, scale * 1.0, scale * 1.0);
+  //glTranslatef(offset_x, offset_y, 0.0);
 
   Axes();
 
@@ -188,8 +192,8 @@ void
 ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
 
-  pix2anglex = 360.0 / (float)horizontal;
-  pix2angley = 360.0 / (float)vertical;
+  auto pix2anglex = 360.0 / (float)horizontal;
+  auto pix2angley = 360.0 / (float)vertical;
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -206,86 +210,136 @@ ChangeSize(GLsizei horizontal, GLsizei vertical)
 }
 
 void
-Mouse(int btn, int state, int x, int y)
+update_lookat()
 {
-  if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-  {
-    x_pos_old = x;
-    y_pos_old = y;
-    status = 1;
-  }
-  else
-  {
-    status = 0;
-  }
+  lookat[0] = std::cos(mouse_horizontal);
+  lookat[1] = std::sin(mouse_vertical);
+  lookat[2] = std::sin(mouse_horizontal);
 }
 
 void
 Motion(GLsizei x, GLsizei y)
 {
+  if (x == 50 && y == 50)
+    return;
+
   delta_x = x - x_pos_old;
   x_pos_old = x;
 
   delta_y = y - y_pos_old;
   y_pos_old = y;
 
+  mouse_horizontal -= delta_x * mouse_sens;
+  mouse_vertical -= delta_y * mouse_sens;
+
+  if (mouse_horizontal > mouse_horizontal_max)
+    mouse_horizontal = mouse_horizontal_max;
+  else if (mouse_horizontal < mouse_horizontal_min)
+    mouse_horizontal = mouse_horizontal_min;
+
+  if (mouse_vertical > mouse_vertical_max)
+    mouse_vertical = mouse_vertical_max;
+  else if (mouse_vertical < mouse_vertical_min)
+    mouse_vertical = mouse_vertical_min;
+
+  update_lookat();
+
   glutPostRedisplay();
+
+  fmt::print("Lookat: {} {} {}\n", lookat[0], lookat[1], lookat[2]);
+
+  if (lock_mouse)
+    glutWarpPointer(50,50);
+}
+
+constexpr void cross3fv(float *a, const float *b)
+{
+  float r[3] {};
+
+  r[0] = a[1]*b[2]-a[2]*b[1];
+  r[1] = a[2]*b[0]-a[0]*b[2];
+  r[2] = a[0]*b[1]-a[1]*b[0];
+
+  a[0] = r[0];
+  a[1] = r[1];
+  a[2] = r[2];
+}
+
+void cross3fv(float vR[], float v1[], float v2[]) {
+  vR[0] =   ( (v1[1] * v2[2]) - (v1[2] * v2[1]) );
+  vR[1] = - ( (v1[0] * v2[2]) - (v1[2] * v2[0]) );
+  vR[2] =   ( (v1[0] * v2[1]) - (v1[1] * v2[0]) );
+}
+
+void norm3fv(float vR[], float v1[]) {
+  float mag;
+
+  mag = sqrt( pow(v1[0], 2) +
+               pow(v1[1], 2) +
+               pow(v1[2], 2)
+             );
+
+  vR[0] = v1[0] / mag;
+  vR[1] = v1[1] / mag;
+  vR[2] = v1[2] / mag;
 }
 
 void
 Keyboard(unsigned char key, int x, int y)
 {
-  constexpr double camera_speed = 1.0;
+  constexpr double camera_speed = 1.0f;
 
   switch (key)
   {
+    case 'm':
+      mouse_sens /= 2.0f;
+      break;
+
+    case 'M':
+      mouse_sens *= 2.0f;
+      break;
+
     case 'l':
-      viewer[0] += camera_speed;
+      lock_mouse = !lock_mouse;
       break;
 
-    case 'h':
-      viewer[0] -= camera_speed;
+    case 'w':
+      viewer[0] += lookat[0] * camera_speed;
+      viewer[1] += lookat[1] * camera_speed;
+      viewer[2] += lookat[2] * camera_speed;
+      update_lookat();
       break;
 
-    case 'k':
-      viewer[1] += camera_speed;
+    case 's':
+      viewer[0] -= lookat[0] * camera_speed;
+      viewer[1] -= lookat[1] * camera_speed;
+      viewer[2] -= lookat[2] * camera_speed;
+      update_lookat();
       break;
 
-    case 'j':
-      viewer[1] -= camera_speed;
+    case 'd': {
+      float crossv[3];
+      float crossvup[3] {viewer[0] + viewer_up[0], viewer[1] + viewer_up[1], viewer[2] + viewer_up[2]};
+      cross3fv(crossv, viewer, crossvup);
+      norm3fv(crossv, crossv);
+      viewer[0] += crossv[0] * camera_speed;
+      viewer[1] += crossv[1] * camera_speed;
+      viewer[2] += crossv[2] * camera_speed;
+      update_lookat();
       break;
+    }
 
-    case 'f':
-      viewer[2] += camera_speed;
+    case 'a': {
+      float crossv[3];
+      float crossvup[3] {viewer[0] + viewer_up[0], viewer[1] + viewer_up[1], viewer[2] + viewer_up[2]};
+      cross3fv(crossv, viewer, crossvup);
+      norm3fv(crossv, crossv);
+      viewer[0] -= crossv[0] * camera_speed;
+      viewer[1] -= crossv[1] * camera_speed;
+      viewer[2] -= crossv[2] * camera_speed;
+      update_lookat();
       break;
-
-    case 'g':
-      viewer[2] -= camera_speed;
-      break;
-
-    case 'X':
-      lookpoint_x += camera_speed;
-      break;
-
-    case 'x':
-      lookpoint_x -= camera_speed;
-      break;
-
-    case 'Y':
-      lookpoint_y += camera_speed;
-      break;
-
-    case 'y':
-      lookpoint_y -= camera_speed;
-      break;
-
-    case 'Z':
-      lookpoint_z += camera_speed;
-      break;
-
-    case 'z':
-      lookpoint_z -= camera_speed;
-      break;
+    }
 
     default:
       break;
@@ -295,7 +349,7 @@ Keyboard(unsigned char key, int x, int y)
     "Position: {} {} {}\n"
     "Lookat: {} {} {}\n",
     viewer[0], viewer[1], viewer[2],
-    lookpoint_x, lookpoint_y, lookpoint_z);
+    lookat[0], lookat[1], lookat[2]);
   RenderScene();
 }
 
@@ -389,8 +443,7 @@ main(int argc, char **argv)
   glutReshapeFunc(ChangeSize);
   MyInit();
   glEnable(GL_DEPTH_TEST);
-  glutMouseFunc(Mouse);
-  glutMotionFunc(Motion);
+  glutPassiveMotionFunc(Motion);
   glutKeyboardFunc(Keyboard);
   glutMainLoop();
 }
