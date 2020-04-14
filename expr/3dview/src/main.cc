b883c4780e6dbb9b78907318f19407d6563cbf25
SecMeant <secmeant@gmail.com>
2020-04-14 14:45:31 +0200

    Adding mouse movement fix and flip hack, wsad fully works, cursor locking
    Fixing some vector calculations so the mouse movement works fine in
    horizontal and vertical axes.
    
    Preventing fliping by offseting mouse_vertical_max and *_min by 0.1.
    There must be better way to do it.
    
    Adding cursor locking. Cursor is also not rendered into the screen.

--- expr/3dview/src/main.cc
+++ expr/3dview/src/main.cc
@@ -1,7 +1,5 @@
-#include <GL/gl.h>
-#include <GL/glut.h>
+#include <cassert>
 #include <cmath>
-#include <fmt/format.h>
 #include <vector>
 
 #include <fcntl.h>
@@ -9,18 +7,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <fmt/format.h>
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+
 typedef float point3[3];
 
-static GLfloat viewer[] = { 0.0, 0.0, 0.0 };
-constexpr static GLfloat viewer_up[] = { 0.0, -1.0, 0.0 };
+static GLfloat viewer[] = { 5.0, 0.0, 0.0 };
+static GLfloat viewer_up[] = { 0.0, 1.0, 0.0 };
 static GLfloat mouse_sens = 1.0f / std::pow(2.0f, 9.0f);
 constexpr static GLfloat mouse_horizontal_max = M_PI * 2.0;
 constexpr static GLfloat mouse_horizontal_min = 0;
-constexpr static GLfloat mouse_vertical_max = M_PI * 2.0;
-constexpr static GLfloat mouse_vertical_min = 0;
+constexpr static GLfloat mouse_vertical_max = M_PI / 2.0 - 0.1;
+constexpr static GLfloat mouse_vertical_min = -(M_PI / 2.0 - 0.1);
 static GLfloat mouse_horizontal = mouse_horizontal_max / 2.0;
-static GLfloat mouse_vertical = mouse_vertical_max / 2.0;
+static GLfloat mouse_vertical =
+  (mouse_vertical_max - mouse_vertical_min) / 2.0;
 static GLfloat lookat[] = { 0.0, 0.0, 1.0f };
+static GLsizei XLOCK_POS = 256, YLOCK_POS = 256;
 static bool lock_mouse = true;
 
 static GLint status = 0;
@@ -39,18 +44,17 @@ static GLfloat offset_y = 0;
 float data_x, data_y, data_width;
 std::vector<float> points;
 
-void
+static void
 Axes(void)
 {
+  point3 x_min = { -50.0, 0.0, 0.0 };
+  point3 x_max = { 50.0, 0.0, 0.0 };
 
-  point3 x_min = { -5.0, 0.0, 0.0 };
-  point3 x_max = { 5.0, 0.0, 0.0 };
+  point3 y_min = { 0.0, -50.0, 0.0 };
+  point3 y_max = { 0.0, 50.0, 0.0 };
 
-  point3 y_min = { 0.0, -5.0, 0.0 };
-  point3 y_max = { 0.0, 5.0, 0.0 };
-
-  point3 z_min = { 0.0, 0.0, -5.0 };
-  point3 z_max = { 0.0, 0.0, 5.0 };
+  point3 z_min = { 0.0, 0.0, -50.0 };
+  point3 z_max = { 0.0, 0.0, 50.0 };
 
   glColor3f(1.0f, 0.0f, 0.0f);
   glBegin(GL_LINES);
@@ -60,12 +64,16 @@ Axes(void)
 
   glEnd();
 
+  glColor3f(0.0f, 0.5f, 0.0f);
+  glBegin(GL_LINES);
+  glVertex3fv(y_min);
+  glVertex3f(0, 0, 0);
+  glEnd();
+
   glColor3f(0.0f, 1.0f, 0.0f);
   glBegin(GL_LINES);
-
-  glVertex3fv(y_min);
   glVertex3fv(y_max);
-
+  glVertex3f(0, 0, 0);
   glEnd();
 
   glColor3f(1.0f, 1.0f, 1.0f);
@@ -79,7 +87,7 @@ Axes(void)
 
 float asdf = 128.0f;
 
-const float *
+static const float *
 matrix_at(const float *mat, size_t x, size_t y, size_t w)
 {
   auto offset = x + y * w;
@@ -87,7 +95,7 @@ matrix_at(const float *mat, size_t x, size_t y, size_t w)
   return pixel_ptr;
 }
 
-void
+static void
 drawpoints(const std::vector<float> &v)
 {
   auto x = 0; // data_x;
@@ -99,59 +107,28 @@ drawpoints(const std::vector<float> &v)
   const float *data = v.data();
   size_t h = v.size() / w;
 
-  glBegin(GL_QUADS);
-  glColor3f(0.75f, 0.0f, 0.35f);
-  glVertex3f(0.0f, 0.0f, 0.0f);
-  glVertex3f(h, 0.0f, 0.0f);
-  glVertex3f(h, 0.0f, w);
-  glVertex3f(0.0f, 0.0f, w);
-  glEnd();
-
   glBegin(GL_POINTS);
   glColor3f(1.0f, 1.0f, 1.0f);
 
   auto cutoff = 0ul;
 
-  for (size_t j = cutoff; j < h - 1 - cutoff; ++j)
+  for (size_t j = 0; j < h - 1 - cutoff; ++j)
     for (size_t i = cutoff; i < w - 1 - cutoff; ++i)
     {
-      //fmt::print("Drawing at {} {} {}\n",
-      //           *matrix_at(data, i, j, w),
-      //           (y + j),
-      //           (x + i));
-      auto depth = *matrix_at(data, i, j, w);
+      auto depth = *matrix_at(data, i, h - 1 - cutoff - j, w);
 
       if (depth == 255.0f)
         continue;
 
-      glVertex3f(depth / scale_factor,
+      glVertex3f((x + i) / scale_factor,
                  (y + j) / scale_factor,
-                 (x + i) / scale_factor);
+                 depth / scale_factor);
     }
-  // for (auto p : v)
-  //{
-  //  if (p == 255.0f || p == 0.0f)
-  //    continue;
-  //  glVertex3f(
-  //    p / scale_factor, (y + j) / scale_factor, (x + i) / scale_factor);
-  //  fmt::print("Drawing at {} {} {}\n",
-  //             p / scale_factor,
-  //             (y + j) / scale_factor,
-  //             (x + i) / scale_factor);
-
-  //  ++i;
-
-  //  if (i == w)
-  //  {
-  //    i = 0;
-  //    ++j;
-  //  }
-  //}
 
   glEnd();
 }
 
-void
+static void
 RenderScene(void)
 {
 
@@ -167,30 +144,19 @@ RenderScene(void)
             viewer_up[0],
             viewer_up[1],
             viewer_up[2]);
-
-  //glRotatef(thetax, 0.0, 1.0, 0.0);
-  //glRotatef(thetay, 1.0, 0.0, 0.0);
-  //glScalef(scale * 1.0, scale * 1.0, scale * 1.0);
-  //glTranslatef(offset_x, offset_y, 0.0);
-
   Axes();
 
   drawpoints(points);
-  // glutWireTeapot(3.0);
 
   glFlush();
   glutSwapBuffers();
 }
 
-void
-MyInit(void)
-{
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-}
-
-void
+static void
 ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
+  XLOCK_POS = horizontal / 2;
+  YLOCK_POS = vertical / 2;
 
   auto pix2anglex = 360.0 / (float)horizontal;
   auto pix2angley = 360.0 / (float)vertical;
@@ -209,7 +175,15 @@ ChangeSize(GLsizei horizontal, GLsizei vertical)
   glLoadIdentity();
 }
 
-void
+static void
+update_viewer_up()
+{
+  viewer_up[0] = 0;
+  viewer_up[1] = std::sin(mouse_vertical + (M_PI / 2.0));
+  viewer_up[2] = 0;
+}
+
+static void
 update_lookat()
 {
   lookat[0] = std::cos(mouse_horizontal);
@@ -217,25 +191,25 @@ update_lookat()
   lookat[2] = std::sin(mouse_horizontal);
 }
 
-void
+static void
 Motion(GLsizei x, GLsizei y)
 {
-  if (x == 50 && y == 50)
+  if (x == XLOCK_POS && y == YLOCK_POS)
     return;
 
-  delta_x = x - x_pos_old;
-  x_pos_old = x;
+  delta_x = x - XLOCK_POS;
+  delta_y = YLOCK_POS - y;
 
-  delta_y = y - y_pos_old;
-  y_pos_old = y;
+  if (lock_mouse)
+    glutWarpPointer(XLOCK_POS, YLOCK_POS);
 
-  mouse_horizontal -= delta_x * mouse_sens;
-  mouse_vertical -= delta_y * mouse_sens;
+  mouse_horizontal += delta_x * mouse_sens;
+  mouse_vertical += delta_y * mouse_sens;
 
   if (mouse_horizontal > mouse_horizontal_max)
-    mouse_horizontal = mouse_horizontal_max;
-  else if (mouse_horizontal < mouse_horizontal_min)
     mouse_horizontal = mouse_horizontal_min;
+  else if (mouse_horizontal < mouse_horizontal_min)
+    mouse_horizontal = mouse_horizontal_max;
 
   if (mouse_vertical > mouse_vertical_max)
     mouse_vertical = mouse_vertical_max;
@@ -243,48 +217,52 @@ Motion(GLsizei x, GLsizei y)
     mouse_vertical = mouse_vertical_min;
 
   update_lookat();
+  update_viewer_up();
 
   glutPostRedisplay();
-
-  fmt::print("Lookat: {} {} {}\n", lookat[0], lookat[1], lookat[2]);
-
-  if (lock_mouse)
-    glutWarpPointer(50,50);
 }
 
-constexpr void cross3fv(float *a, const float *b)
+constexpr static void
+cross3fv(float *a, const float *b)
 {
-  float r[3] {};
+  float r[3]{};
 
-  r[0] = a[1]*b[2]-a[2]*b[1];
-  r[1] = a[2]*b[0]-a[0]*b[2];
-  r[2] = a[0]*b[1]-a[1]*b[0];
+  r[0] = a[1] * b[2] - a[2] * b[1];
+  r[1] = a[2] * b[0] - a[0] * b[2];
+  r[2] = a[0] * b[1] - a[1] * b[0];
 
   a[0] = r[0];
   a[1] = r[1];
   a[2] = r[2];
 }
 
-void cross3fv(float vR[], float v1[], float v2[]) {
-  vR[0] =   ( (v1[1] * v2[2]) - (v1[2] * v2[1]) );
-  vR[1] = - ( (v1[0] * v2[2]) - (v1[2] * v2[0]) );
-  vR[2] =   ( (v1[0] * v2[1]) - (v1[1] * v2[0]) );
+static void
+cross3fv(float vR[], float v1[], float v2[])
+{
+  vR[0] = ((v1[1] * v2[2]) - (v1[2] * v2[1]));
+  vR[1] = -((v1[0] * v2[2]) - (v1[2] * v2[0]));
+  vR[2] = ((v1[0] * v2[1]) - (v1[1] * v2[0]));
 }
 
-void norm3fv(float vR[], float v1[]) {
+static float
+dot3fv(float v1[], float v2[])
+{
+  return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+
+static void
+norm3fv(float vR[], float v1[])
+{
   float mag;
 
-  mag = sqrt( pow(v1[0], 2) +
-               pow(v1[1], 2) +
-               pow(v1[2], 2)
-             );
+  mag = sqrt(pow(v1[0], 2) + pow(v1[1], 2) + pow(v1[2], 2));
 
   vR[0] = v1[0] / mag;
   vR[1] = v1[1] / mag;
   vR[2] = v1[2] / mag;
 }
 
-void
+static void
 Keyboard(unsigned char key, int x, int y)
 {
   constexpr double camera_speed = 1.0f;
@@ -308,6 +286,7 @@ Keyboard(unsigned char key, int x, int y)
       viewer[1] += lookat[1] * camera_speed;
       viewer[2] += lookat[2] * camera_speed;
       update_lookat();
+      update_viewer_up();
       break;
 
     case 's':
@@ -315,29 +294,30 @@ Keyboard(unsigned char key, int x, int y)
       viewer[1] -= lookat[1] * camera_speed;
       viewer[2] -= lookat[2] * camera_speed;
       update_lookat();
+      update_viewer_up();
       break;
 
     case 'd': {
       float crossv[3];
-      float crossvup[3] {viewer[0] + viewer_up[0], viewer[1] + viewer_up[1], viewer[2] + viewer_up[2]};
-      cross3fv(crossv, viewer, crossvup);
+      cross3fv(crossv, lookat, viewer_up);
       norm3fv(crossv, crossv);
       viewer[0] += crossv[0] * camera_speed;
       viewer[1] += crossv[1] * camera_speed;
       viewer[2] += crossv[2] * camera_speed;
       update_lookat();
+      update_viewer_up();
       break;
     }
 
     case 'a': {
       float crossv[3];
-      float crossvup[3] {viewer[0] + viewer_up[0], viewer[1] + viewer_up[1], viewer[2] + viewer_up[2]};
-      cross3fv(crossv, viewer, crossvup);
+      cross3fv(crossv, lookat, viewer_up);
       norm3fv(crossv, crossv);
       viewer[0] -= crossv[0] * camera_speed;
       viewer[1] -= crossv[1] * camera_speed;
       viewer[2] -= crossv[2] * camera_speed;
       update_lookat();
+      update_viewer_up();
       break;
     }
 
@@ -345,15 +325,10 @@ Keyboard(unsigned char key, int x, int y)
       break;
   }
 
-  fmt::print(
-    "Position: {} {} {}\n"
-    "Lookat: {} {} {}\n",
-    viewer[0], viewer[1], viewer[2],
-    lookat[0], lookat[1], lookat[2]);
   RenderScene();
 }
 
-void
+static void
 usage(int argc, char **argv)
 {
   fmt::print("Usage: {} <raw data file>\n", argv[0]);
@@ -437,13 +412,14 @@ main(int argc, char **argv)
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(300, 300);
-  glutCreateWindow("Rzutowanie perspektywiczne");
+  glutCreateWindow("kinect 3dview");
 
   glutDisplayFunc(RenderScene);
   glutReshapeFunc(ChangeSize);
-  MyInit();
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glEnable(GL_DEPTH_TEST);
   glutPassiveMotionFunc(Motion);
   glutKeyboardFunc(Keyboard);
+  glutSetCursor(GLUT_CURSOR_NONE);
   glutMainLoop();
 }
