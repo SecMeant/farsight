5ec4621b623e3fd8666977ba27653b540def07cc
windspring <sagan.pawel1000@gmail.com>
2020-05-29 10:49:21 +0200

    backup

--- expr/aruco_dump.cc
+++ expr/aruco_dump.cc
@@ -20,14 +20,35 @@ int main()
   cv::VideoCapture inputVideo;
   inputVideo.open(0); 
   char c;
+  bool success = false;
   while(c != 'q')
   {
       inputVideo.grab();
       cv::Mat image;
       inputVideo.retrieve(image);
       if( c == 'c')
-          cv::imwrite(fmt::format("../../../aruco/frame{}.jpg", frame_nr++),image);
+      {
+          while(not success)
+          {
+              cv::Mat f = cv::imread(images[i]);
+              cv::cvtColor(f,f,cv::COLOR_BGR2GRAY);
+
+            // Finding checker board corners
+            // If desired number of corners are found in the image then success = true  
+            success = cv::findChessboardCorners(f, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FAST_CHECK | cv::CALIB_CB_NORMALIZE_IMAGE);
+            if(success)
+            {
+              cv::TermCriteria criteria(cv::TermCriteria::COUNT+cv::TermCriteria::EPS, 30, 0.001);
+              
+              // refining pixel coordinates for given 2d points.
+              cv::cornerSubPix(f,corner_pts,cv::Size(11,11), cv::Size(-1,-1),criteria);
+              
+              // Displaying the detected corner points on the checker board
+              cv::drawChessboardCorners(f, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, success);
+              cv::imwrite(fmt::format("../../../aruco/frame{}.jpg", frame_nr++),image);
+            }
+          }
+      }
       cv::imshow("out",image);
       c = (char)cv::waitKey(waitTime);
-  }
 }
