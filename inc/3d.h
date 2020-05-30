0b657169a8cb82f0b236f3d541fb0f34f9f4f1f8
SecMeant <secmeant@gmail.com>
2020-05-30 15:18:08 +0200

    [3dview] camera points update now also triggers rerender

--- inc/3d.h
+++ inc/3d.h
@@ -9,6 +9,8 @@ namespace farsight {
 
   extern Context3D context3D;
 
+  void RenderScene();
+
   void
   init3d();
 
@@ -16,12 +18,14 @@ namespace farsight {
   update_points_cam1(std::vector<farsight::Point3f> points, size_t width)
   {
     context3D.update_cam1(std::move(points), width);
+    RenderScene();
   }
 
   inline void
   update_points_cam2(std::vector<farsight::Point3f> points, size_t width)
   {
     context3D.update_cam2(std::move(points), width);
+    RenderScene();
   }
 
 } // namespace farsight
--- src/3dview.cc
+++ src/3dview.cc
@@ -149,8 +149,8 @@ drawpoints(const CameraShot &cs)
   glEnd();
 }
 
-static void
-RenderScene(void)
+void
+RenderScene()
 {
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
